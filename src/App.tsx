827beb1ac7470c3e1d834915a0c11bef3f492ddf
Johinner Madariaga <johinner5721@gmail.com>
2024-03-09 20:49:38 -0500

    'home'

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Header, Home } from './components'
+
+function App() {
+ 
+  return (
+  <>
+    <Header/>
+    
+    <main className="main">
+      <Home/>
+    </main>
+  </>
+  )
+}
+
+export default App
---
+++ src/components/home/components/Social.tsx
@@ -0,0 +1,18 @@
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://github.com/johinnerMC" className="home__social-icon" target="_blank">
+            <i className="uil uil-github"></i>
+        </a>
+
+        <a href="https://discord.com" className="home__social-icon" target="_blank">
+             <i className="uil uil-discord"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/johinner-madariaga-7595022a2/" className="home__social-icon" target="_blank">
+             <i className="uil uil-linkedin"></i>
+        </a>
+    </div>
+  )
+}
