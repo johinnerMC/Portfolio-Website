b9ebce4f65d639e9c356b39a81c499ce4434ab78
Johinner Madariaga <johinner5721@gmail.com>
2024-07-21 19:09:06 -0500

    21/07/24

--- src/pages/writingPage/WritingPage.tsx
+++ src/pages/writingPage/WritingPage.tsx
@@ -1,23 +1,30 @@
 /** @format */
 import "./writingPage.css";
 import { CodeHighlighter } from "./components";
+//import styled from "styled-components";
 
 export const WritingPage = () => {
-  const codeString = `
-  import { Data, ScrollDown, Social } from './components'
-    import './home.css'
-
-
-    export const Home = () => {
-  return (
-    <section className="home section" id="home">
-
-}`;
+  const codeHijoDirectos = `
+<ul class="hijos-directos">
+      <li><b>Primavera</b></li>
+      <li><b>Verano</b></li>
+      <li><b>Otoño</b></li>
+</ul>`;
   return (
     <section className="section">
-      {<CodeHighlighter codeString={codeString} language="tsx" />}
-      <h1>CSS</h1>
-      {<CodeHighlighter codeString={codeString} language="php" />}
+      <h2 className="section__title">Aprendiendo CSS</h2>
+      <span className="section__subtitle">Cascading Style Sheets</span>
+
+      <article className="writing__content">
+        <h3 className="article__title">Selectores hijos</h3>
+        <span className="article__subtitle">Hijos directos</span>
+        <p className="article__description">
+          El selector Estila el elemento en primer nivel dentro de su clase
+          seleccionada
+        </p>
+
+        {<CodeHighlighter codeString={codeHijoDirectos} language="html" />}
+      </article>
     </section>
   );
 };
